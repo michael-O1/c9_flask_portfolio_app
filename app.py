f6a8781553120fbfd25eb5b1e21fe96c77141fe7
michael-O1 <52648205+michael-O1@users.noreply.github.com>
2019-09-11 11:21:28 +0800

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	return render_template('index.html', name=name)
+	new_name = name + 'likes to eat apples'
+	return render_template('index.html', new_name=name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
